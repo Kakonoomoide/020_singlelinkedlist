535413c4c6407e434619b0d480b918e2c178f174
kizaru <rizkiramada128@gmail.com>
2023-05-18 12:57:55 +0700

    membuat methode untuk menambahkan nilai ke dalam variable nim dan nama

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -9,7 +9,19 @@ struct Node{
 
 Node* start = null;
 
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
 
+	cout << "Masukan Nim  : ";
+	cin >> nim;
+	cout << "Masukan nama : ";
+	cin >> nama;
+
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+}
 
 int main(){
 
