821c64c48c068a9c51d1262de4461c1928b33fe6
kizaru <rizkiramada128@gmail.com>
2023-05-18 13:22:53 +0700

    membuat program untuk memindahkan previous dan current

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -51,6 +51,10 @@ bool searchNode(int nim, Node* current, Node* previous) {
 	previous = START;
 	current = START;
 
+	while (current != NULL && nim > current->noMhs){
+		previous = current;
+		current = current->next;
+	}
 }
 
 int main(){
