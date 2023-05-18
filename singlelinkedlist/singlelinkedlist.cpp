aa168676ef291603dca56b62d6ed51cf6612982f
kizaru <rizkiramada128@gmail.com>
2023-05-18 14:12:39 +0700

    memanggil semuat function dan methode kedalam program utama, menggunakan loping dan switch case

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -24,7 +24,7 @@ void addNode() {
 
 	if (START == NULL || nim <= START->noMhs) {
 		if (START != NULL && nim == START->noMhs) {
-			cout << "Nim sudah ada!" << endl;
+			cout << "\nNim sudah ada!" << endl;
 			return;
 		}
 		nodeBaru->next = START;
@@ -37,7 +37,7 @@ void addNode() {
 
 	while ((current != NULL) && (nim >= current->noMhs)){
 		if (nim == current->noMhs) {
-			cout << "Nim sudah ada!" << endl;
+			cout << "\nNim sudah ada!" << endl;
 			return;
 		}
 		previous = current;
@@ -126,5 +126,61 @@ void searchData() {
 }
 
 int main(){
+	int pilihan;
+	do{
+		try {
 
+			cout << "1. Menambahkan Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tanpilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+
+			cout << "Pilihan : ";
+			cin >> pilihan;
+
+			switch (pilihan) {
+			case 1:
+				addNode();
+				cout << "data berhasil ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				if (listEmpty()) {
+					cout << "list kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+
+				cout << "masukan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "NIM: " << nim << "berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "data tidak ditemukan" << endl;
+				break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "pilihan tidak ada" << endl;
+				break;
+			}
+		}
+		catch (exception e) {
+			cout << "Terjadi Kesalahan" << endl;
+		}
+	} while (pilihan != 5);
 }
